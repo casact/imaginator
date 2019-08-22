e4fefd1e2e3d96ba17febbfb349eb0b9ff601bca
kevinykuo <kevinykuo@gmail.com>
2019-08-22 14:53:38 -0400

    fix tests

--- tests/testthat/test-ClaimsByFirstReport.R
+++ tests/testthat/test-ClaimsByFirstReport.R
@@ -5,16 +5,16 @@ test_that("1st report", {
   dfPolicy <- NewPolicyYear(100, 2001)
 
   dfClaims <- ClaimsByFirstReport(dfPolicy
-                                  , Frequency = FixedHelper(10:7)
-                                  , PaymentSeverity = FixedHelper(100 * 1:4)
+                                  , Frequency = 10:7
+                                  , PaymentSeverity = 100 * 1:4
                                   , Lags = 1:4)
 
   testthat::expect_equal(nrow(dfClaims), 100 * sum(10:7))
 
   dfClaims <- ClaimsByFirstReport(
       dfPolicy
-    , Frequency = FixedHelper(10)
-    , PaymentSeverity = FixedHelper(100)
+    , Frequency = 10
+    , PaymentSeverity = 100
     , Lags = 1)
 
   testthat::expect_equal(nrow(dfClaims), 100 * 10)
