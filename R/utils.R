fcab8ae6718bf1cdfbc016ec79172c7123bd7977
Brian Fannin <bfannin@casact.org>
2020-06-10 14:51:13 -0400

    License -> MPL2

--- R/utils.R
+++ R/utils.R
@@ -1,3 +1,6 @@
+# This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+# If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 sample_or_rep <- function(x, n) {
   if (distributions3::is_distribution(x))
     distributions3::random(x, n)
